ce0ded27353cee92de5b45cf0ae40293ee897f0e
keith r. bergerstock <krbergerstock@e4kountdown.com>
2022-11-04 23:20:08 -0400

    first commit

--- sieve.py
+++ sieve.py
@@ -9,36 +9,37 @@ import bitarray
 
 
 M = {
-    10 : 4,
-    100 : 25,
-    1000 : 168,
-    10000 : 1229,
-    100000 : 9592,
-    1000000 : 78498,
-    10000000 : 664579,
-    100000000 : 5761455,
+    10: 4,
+    100: 25,
+    1000: 168,
+    10000: 1229,
+    100000: 9592,
+    1000000: 78498,
+    10000000: 664579,
+    100000000: 5761455,
 }
 
 
-def milliseconds(t0,t2):
+def milliseconds(t0, t2):
     return (t2 - t0) * 1000
 
 
 class Primes:
-    def __init__(self,max):
+    def __init__(self, max):
         self.n = max
         self.nl = max // 2
         self.v = bitarray.bitarray(self.nl)
         self.v[0:self.nl] = 0
 
     def get_primes(self):
-        """ returns an array of prime numbers based on the results in the sieve array """
+        """ returns an array of prime numbers
+            based on the results in the sievearray """
         p = [2]
         cnt = 0
         for k in self.v:
             if k == 0:
                 p.append(1 + (cnt * 2))
-            cnt += 1   
+            cnt += 1
         return p
 
     def sieve2(self):
@@ -54,18 +55,19 @@ class Primes:
         for j in range(1, self.nl):
             if self.v[j] == 0:
                 # calc the factor
-                f = 1 + (j *2)
+                f = 1 + (j * 2)
                 f2 = f * f
                 # equivlent to f < sqrt(n)
                 if f2 < self.n:
-                    # mark all multiples of prime starting at the factored squared
+                    # mark all multiples of prime
+                    # starting at the factored squared
                     for ndx in range((f2 - 1) // 2, self.nl, f):
                         # as not prime
-                        self.v[ndx] = 1 
-                else: 
+                        self.v[ndx] = 1
+                else:
                     # all multiples have been marked so stop
-                    break 
-                    
+                    break
+
     def counted(self):
         """ returns the number of counted primes """
         # adjust for not including TWO as a factor
@@ -76,10 +78,10 @@ class Primes:
         return self.counted() == M[self.n]
 
 
-# main 
+# main
 def time_sieve(prime_limit, time_limit, output):
     cnt = 0                         # pass counterf
-    primes = Primes(prime_limit)    # the sieve 
+    primes = Primes(prime_limit)    # the sieve
     duration = 0
     print("--n{}".format(prime_limit))
     print('--t{}'.format(time_limit))
@@ -89,21 +91,23 @@ def time_sieve(prime_limit, time_limit, output):
         t1 = t0 = time.perf_counter()
         primes.sieve2()
         t1 = time.perf_counter()
-        duration += milliseconds(t0,t1)
+        duration += milliseconds(t0, t1)
         cnt += 1
         if not primes.validate():
             break
 
-    print("passes: {0:4d}  time: {1:9.4f} Ms Avg: {2:7.4f} Ms  Limit: {3:8d}  count: {4:4d} valid: {5}"\
-        .format(cnt,duration,duration/cnt,prime_limit,primes.counted(), primes.validate() ))
+    print("passes: {0:4d}  time: {1:9.4f} Ms Avg: {2:7.4f} Ms Limit: {3:8d}  count: {4:4d} valid: {5}\n"
+          .format(cnt, duration, duration/cnt, prime_limit,
+             primes.counted(), primes.validate()))
 
     if output:
-        print(primes.get_primes())  
+        print(primes.get_primes())
 
-                
+
 if __name__ == "__main__":
     prime_limit = 1000000
-    time_limit = 10000      #time unit is in mS
+    # time unit is in mS
+    time_limit = 10000
     output = False
 
     for argc in sys.argv:
